606c0554136c27eb327faa083c66278a24c3ab12
Raivydas Šimėnas <raivydas.simenas@proton.me>
2023-11-28 19:15:34 +0200

    Invader movement

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
+const figureHeight = 25;
+const figureWidth = 25;
+const invaderSpeed = 5;
+const canvas = document.querySelector("#game-canvas");
+canvas.width = 200;
+canvas.height = 400;
+
 $(document).ready(function() {
-    const canvas = document.querySelector("#game-canvas");
-    canvas.width = 200;
-    canvas.height = 400;
+    
     const ctx = canvas.getContext('2d');
     initializeCanvas(ctx);
 
 });
 
 function initializeCanvas(ctx) {
-    const player = new Image();
+    const image = new Image();
 
-    player.src = "./assets/Space\ invaders\ icon.png";
-    player.height = 10;
-    player.width = 10;
-    // player.classList.add('.player');
-    // player.onload = function() {
-    //     ctx.drawImage(player, 0, 0, player.width, player.height);
-    // }
+    image.src = "./assets/Space\ invaders\ icon.png";
+    image.height = figureHeight;
+    image.width = figureWidth;
+
+    const player = {
+        image,
+        x: 0,
+        y: 375
+    }
+
+    player.image.onload = function() {
+        ctx.drawImage(player.image, player.x, player.y, figureWidth, figureHeight);
+    }
 
     const invaders = [];
     for (let i = 0; i < 40; i++) {
-        invaders.push(new Image());
-        invaders[invaders.length - 1].src = "./assets/Alien\ icon.png";
-        invaders[invaders.length - 1].height = 20;
-        invaders[invaders.length - 1].width = 20;
+        const image = new Image();
+        image.src = "./assets/Alien\ icon.png";
+        image.height = figureHeight;
+        image.width = figureWidth;
+        const invader = {
+            image,
+            x: figureWidth * (i % 8),
+            y: figureHeight * (Math.floor(i / 8))
+        }
+        invaders.push(invader);
     }
 
     let loadedImages = 0;
-    invaders.forEach(function(image) {
-        image.onload = function() {
+    invaders.forEach(function(invader) {
+        invader.image.onload = function() {
             loadedImages++;
             if (loadedImages === invaders.length) {
                 drawInvaders(invaders, ctx);
@@ -36,11 +53,30 @@ function initializeCanvas(ctx) {
         }
     })
 
-    function drawInvaders(invaders, ctx) {
-        invaders.forEach(function(image, index) {
-            const x = 20 * (index % 8);
-            const y = 20 * (Math.floor(index / 8));
-            ctx.drawImage(image, x, y, 20, 20);
+    function drawInvaders() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        invaders.forEach(function(invader) {
+            ctx.drawImage(invader.image, invader.x, invader.y, figureWidth, figureHeight);
         })
     }
+
+    function moveInvaders() {
+        invaders.forEach(invader => {
+            if (invader.x + 10 > canvas.length) {
+                invader.x = 0;
+                invader.y += 25;
+            } else {
+                invader.x += 5;
+            }
+        })
+    }
+
+    function update() {
+        moveInvaders();
+        drawInvaders()
+    }
+
+    (function playGame() {
+        setInterval(update, 1000 / 30);
+    })();
 }
